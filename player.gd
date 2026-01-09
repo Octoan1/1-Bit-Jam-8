3945390f47c62b44ee3cf35eeba83ca29fbd5c97
Octoan1 <okloepping@gmail.com>
2026-01-09 14:36:49 -0600

    better jump controler
    - added coyote time
    - and jump king style jump

--- player.gd
+++ player.gd
@@ -6,6 +6,9 @@ extends CharacterBody2D
 @export var SPEED = 100.0
 @export var JUMP_VELOCITY = -200.0
 @export var CUT_JUMP_VELOCITY = -10
+var vel_storage := 0
+var can_jump : bool = true
+var gravity
 
 
 func _ready() -> void:
@@ -16,13 +19,21 @@ func _physics_process(delta: float) -> void:
 	
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * 0.9 * delta
+		print(get_gravity())
+		velocity += get_gravity() * 0.6 * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump"): #and is_on_floor():
+	if is_on_floor():
+		can_jump = true
+		
+	if not is_on_floor() and can_jump and $CoyoteTimer.is_stopped():
+		$CoyoteTimer.start()
+	
+	if Input.is_action_pressed("jump") and can_jump:
+		add_jump()
+		print(vel_storage)
+	if Input.is_action_just_released("jump") and can_jump:
 		jump()
-	if Input.is_action_just_released("jump"):
-		cut_jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -39,9 +50,20 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func jump() -> void:
-	self.velocity.y = JUMP_VELOCITY
+	self.velocity.y = vel_storage
+	vel_storage = 0
+	can_jump = false
 
+func add_jump() -> void:
+	if vel_storage > JUMP_VELOCITY:
+		vel_storage -= 4
+
+# === UNUSED ===
 func cut_jump() -> void:
 	# When the jump is going upwards cut its velocity
 	if self.velocity.y < CUT_JUMP_VELOCITY:
 		self.velocity.y = CUT_JUMP_VELOCITY
+# === === ===
+
+func _on_coyote_timer_timeout() -> void:
+	can_jump = false
